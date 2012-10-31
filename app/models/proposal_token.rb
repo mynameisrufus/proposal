d81fefa11d25460c3f06f00cefc23c66722bea7c
jgalilee <jack.galilee@trineo.com.au>
2012-11-01 10:18:42 +1100

    improved code formatting

--- app/models/proposal_token.rb
+++ app/models/proposal_token.rb
@@ -1,63 +1,41 @@
 class ProposalToken < ActiveRecord::Base
 
-  belongs_to :resource, polymorphic: true
+  belongs_to :resource,
+    polymorphic: true
 
-  class ArgumentsValidator < ActiveModel::Validator
-    def validate_expected record, sym
-      record.errors.add :arguments, "is missing #{sym}" unless
-        record.arguments[sym].present?
-    end
-
-    def validate record
-      if record.expects.is_a? Proc
-        record.errors.add :arguments, "is invalid" unless
-          record.expects.call(record.arguments)
-      elsif record.arguments.is_a? Hash
-        case record.expects
-        when Symbol
-          validate_expected record, record.expects
-        when Array
-          record.expects.each { |sym| validate_expected record, sym }
-        end
-      else
-        record.errors.add :arguments, "must be a hash"
-        case record.expects
-        when Symbol
-          record.errors.add :arguments, "is missing #{record.expects}"
-        when Array
-          record.expects.each do |sym|
-            record.errors.add :arguments, "is missing #{sym}"
-          end
-        end
-      end
-    end
-  end
-
-  class EmailValidator < ActiveModel::Validator
-    def validate record
-      record.errors.add :email, "is not valid" unless
-        record.email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-    end
-  end
+  attr_accessible :email,
+    :proposable,
+    :proposable_type,
+    :expires,
+    :expect,
+    :resource,
+    :args
 
   attr_accessor :expects
 
-  attr_accessible :email, :proposable, :proposable_type, :expires, :expect,
-                  :resource, :args
+  validates_presence_of :email,
+    :token,
+    :proposable,
+    :proposable_type,
+    :expires_at
 
-  validates_presence_of :email, :token, :proposable, :proposable_type,
-                        :expires_at
-
-  validates_with ArgumentsValidator, if: -> { expects.present? }
+  validates_with ArgumentsValidator, if: -> {
+    expects.present?
+  }
 
   validates_with EmailValidator
 
   serialize :arguments
 
-  validates :email, uniqueness: {
-    scope: [:proposable_type, :resource_type, :resource_id],
-    message: "already has an outstanding proposal"
-  }
+  validates :email,
+    uniqueness: {
+      scope: [
+        :proposable_type,
+        :resource_type,
+        :resource_id
+      ],
+      message: "already has an outstanding proposal"
+    }
 
   before_validation on: :create do
     self.token = SecureRandom.base64(15).tr('+/=lIO0', 'pqrsxyz')
@@ -88,7 +66,8 @@ class ProposalToken < ActiveRecord::Base
     constraints = options.slice :email, :proposable_type
     resource = options[:resource]
     if !resource.nil? && resource.respond_to?(:id)
-      constraints.merge! resource_type: resource.class.to_s, resource_id: resource.id
+      constraints.merge! resource_type: resource.class.to_s,
+        resource_id: resource.id
     end
     token = where(constraints).first
     token.nil? ? new(options) : token
@@ -96,8 +75,8 @@ class ProposalToken < ActiveRecord::Base
 
   def to resource
     self.class.find_or_new email: self.email,
-                           proposable_type: self.proposable_type,
-                           resource: resource
+      proposable_type: self.proposable_type,
+      resource: resource
   end
 
   def args= args_array
@@ -111,9 +90,12 @@ class ProposalToken < ActiveRecord::Base
 
   def action
     case
-      when persisted? then :remind
-      when recipient.nil? then :invite
-      else :notify
+      when persisted?
+        :remind
+      when recipient.nil?
+        :invite
+      else
+        :notify
     end
   end
 
