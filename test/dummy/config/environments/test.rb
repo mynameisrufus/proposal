542320601ff573f71094dde133c3b82401e8a080
Janusz Mordarski <janusz.m@gmail.com>
2022-10-26 13:41:44 +0200

    Update test dummy setup

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -1,4 +1,4 @@
-Dummy::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
@@ -25,11 +25,6 @@ Dummy::Application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
-
-  # Tell Action Mailer not to deliver emails to the real world.
-  # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :test
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
